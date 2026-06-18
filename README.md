279ae978e202286243ccbd237569a453a1b6de1f
Skyler Blue Spillers <skylerblue333@users.noreply.github.com>
2026-06-18 22:54:11 +0000

    Initial commit: Production-ready implementation

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sol-ERC20-Token
+
+## Overview
+A standard ERC20 token implemented using OpenZeppelin's secure contracts, featuring minting and burning capabilities.
+
+## Quick Start (1-Click Build)
+
+```bash
+git clone https://github.com/skylerblue333/Sol-ERC20-Token.git
+cd Sol-ERC20-Token
+npm install
+npx hardhat compile
+```
---
+++ contracts/Token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+contract EnterpriseToken is ERC20, ERC20Burnable, Ownable {
+    constructor(address initialOwner) 
+        ERC20("Enterprise Token", "ENT") 
+        Ownable(initialOwner) 
+    {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
