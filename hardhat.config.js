07c4e8d1e4589076e6436d49ddfeb944809e33dd
Skyler Blue Spillers <92972770+skylerblue333@users.noreply.github.com>
2026-08-24 22:26:29 -0500

    build: align Hardhat with contract compiler

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,14 @@
-module.exports = { solidity: '0.8.19' };
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-ethers");
+
+module.exports = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+};
